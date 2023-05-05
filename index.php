550e1a69f3d2fd8a8a6277ba1460553e1d658091
lucaruboni <lucaruboni@gmail.com>
2023-05-05 17:59:28 +0200

    [fix] colors

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ else{
     
         }
         .text_container{
-            color: white;
+            color: black;
             font-weight: 700;
         }
         .my_col{
@@ -105,7 +105,7 @@ else{
     <title>password generator</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
 </head>
-    <body>
+    <body class="bg-black">
     
         <div class="app">
             
